c705319e65f9c39996e90d643ba484b800bdb9a5
VoytovichKlimentiy <Tyuiu.VoytovichKA@outlook.com>
2026-09-16 00:16:03 +0500

    Task1

--- Tyuiu.VoytovichKA.Sprint5.Task1.V9/Program.cs
+++ Tyuiu.VoytovichKA.Sprint5.Task1.V9/Program.cs
@@ -10,7 +10,7 @@ namespace Tyuiu.VoytovichKA.Sprint5.Task0.V27
             Console.Title = "Спринт #4 | Выполнил: Войтович К.А. | НТм-25-1";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #5                                                               *");
-            Console.WriteLine("* Тема: Класс File. Запись набора данных в текстовый файл                  *");
+            Console.WriteLine("* Тема: Класс File. Запись набора данных в текстовый файл                 *");
             Console.WriteLine("* Задание #1                                                              *");
             Console.WriteLine("* Вариант #9                                                              *");
             Console.WriteLine("* Выполнил: Войтович Климентий Антонович | НТм-25-1                       *");
