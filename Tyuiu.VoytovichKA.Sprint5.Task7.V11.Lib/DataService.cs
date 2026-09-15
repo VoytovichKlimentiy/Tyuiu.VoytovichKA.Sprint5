e995fad03428a062592077c926d39ef88626905d
VoytovichKlimentiy <Tyuiu.VoytovichKA@outlook.com>
2026-09-16 03:50:17 +0500

    Task7

---
+++ Tyuiu.VoytovichKA.Sprint5.Task7.V11.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.VoytovichKA.Sprint5.Task7.V11.Lib
+{
+    public class DataService : ISprint5Task7V11
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string outpath=Path.Combine(Directory.GetCurrentDirectory(), "OutPutDataFileTask7V11.txt");
+            if (File.Exists(outpath)) File.Delete(outpath);
+
+            string text = File.ReadAllText(path), res="";
+
+            foreach (char c in text)
+            {
+                if (!((c>='а' && c <= 'я') || c==' ' || c=='ё')) res += c;
+            }
+            File.WriteAllText(outpath, res);
+            return outpath;
+        }
+    }
+}
---
+++ Tyuiu.VoytovichKA.Sprint5.Task7.V11.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.VoytovichKA.Sprint5.Task7.V11.Lib;
+
+namespace Tyuiu.VoytovichKA.Sprint5.Task7.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void IsValid()
+        {
+            DataService ds = new DataService();
+            string inpath = Path.Combine(Directory.GetCurrentDirectory(), "InPutDataFileTask7V11.txt");
+            ds.LoadDataAndSave(inpath);
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "OutPutDataFileTask7V11.txt");
+            bool fe = File.Exists(path);
+            Assert.IsTrue(fe);
+        }
+    }
+}
